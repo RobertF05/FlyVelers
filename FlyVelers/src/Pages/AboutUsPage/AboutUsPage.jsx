65287331feef657830dfa996d4cd149897ca0ee7
RobertF05 <rfuentesf05@gmail.com>
2026-03-19 10:03:29 -0600

    Redirigido el Main

---
+++ FlyVelers/src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutUsPage.css';
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>Página Sobre Nosotros</h1>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ FlyVelers/src/Pages/ContactUsPage/ContactUsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContactUsPage.css';
+
+const ContactUsPage = () => {
+  return (
+    <div>
+      <h1>Página de Contacto</h1>
+    </div>
+  );
+};
+
+export default ContactUsPage;
